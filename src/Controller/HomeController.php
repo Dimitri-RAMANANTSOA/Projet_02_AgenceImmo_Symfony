815b35d3bf60c160d21e36f8c6cda3cc6e3354a7
Dimitri RAMANANTSOA <dimitri.rama.pro@gmail.com>
2022-07-07 09:30:11 +0200

    feat: frontend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,8 +13,11 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-    public function index() : Response
+    public function index(PropertyRepository $repository) : Response
     {
-        return $this->render(view:'pages/home.html.twig');
+        $properties = $repository->findLatest();
+        return $this->render(view:'pages/home.html.twig', parameters: [
+            'properties' => $properties
+        ]);
     }
 }
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,19 +2,69 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class PropertyController extends AbstractController
 {
+    private $repository;
+
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * @Route ("/biens", name="property.index")
      */
     public function index() : Response
     {
+    /*
+        $property= new Property();
+        $property
+            ->setTitle('Mon premier bien')
+            ->setDescription('ma description')
+            ->setSurface('40')
+            ->setRooms('4')
+            ->setBedroom('1')
+            ->setFloor('1')
+            ->setPrice('200000')
+            ->setHeat('1')
+            ->setCity('Villiers le mahieu')
+            ->setAddress('4 rue des 24 arpents')
+            ->setPostalCode('78770');
+
+        $em = $doctrine->getManager();
+        $em->persist($property);
+        $em->flush();
+    */
+
         return $this->render(view:'property/index.html.twig',parameters: [
             'current_menu' => 'properties'
         ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug" : "[a-z0-9\-]*"})
+     * @param Property $property
+     * @return Response
+     */
+    public function show(Property $property, string $slug): Response
+    {
+        $s = $property->getSlug();
+        if($s !== $slug)
+        {
+            return $this->redirectToRoute('property.show',[
+                'id' => $property->getId(),
+                'slug' => $s
+            ], 301);
+        }
+        return $this->render(view:'property/show.html.twig', parameters:[
+            'property' => $property,
+            'current_menu' => 'properties']);
     }
 }
