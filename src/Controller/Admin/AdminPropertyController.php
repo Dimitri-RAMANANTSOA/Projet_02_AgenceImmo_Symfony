43e647560036ed590ed08476c225c516b1047d27
Dimitri RAMANANTSOA <dimitri.rama.pro@gmail.com>
2022-07-07 11:57:07 +0200

    feat:CRUD property

---
+++ src/Controller/Admin/AdminPropertyController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Property;
+use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminPropertyController extends AbstractController
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, ManagerRegistry $doctrine)
+    {
+        $this->repository = $repository;
+        $this->em = $doctrine->getManager();
+    }
+
+    /**
+     * @Route("/admin", name="admin.property.index")
+     * @return Response
+     */
+    public function index() : Response
+    {
+        $properties = $this->repository->findAll();
+        return $this->render('admin/property/index.html.twig', parameters:[
+            'properties' => $properties
+        ]);
+    }
+
+    /**
+     * @Route("/admin/property/edit/{id}", name="admin.property.edit", methods="POST|GET")
+     */
+    public function edit(Property $property, Request $request)
+    {
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush();
+            $this->addFlash('success', 'Le bien a été modifié avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+        return $this->render('admin/property/edit.html.twig', parameters:[
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/property/new", name="admin.property.new")
+     */
+    public function new(Request $request)
+    {
+        $property = new Property();
+
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->persist($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Le bien a été crée avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/new.html.twig', parameters:[
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/property/delete/{id}", name="admin.property.delete", methods="DELETE")
+     */
+    public function delete(Property $property, Request $request)
+    {
+        if($this->isCsrfTokenValid('delete'. $property->getId(), $request->get('_token')))
+        {
+            $this->em->remove($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Le bien a été supprimé avec succès');  
+        }
+
+        return $this->redirectToRoute('admin.property.index');
+    }
+}
